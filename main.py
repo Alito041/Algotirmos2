8fcebb190798169c41dbc1d2d7aaba65750a9290
Alito041 <153631342+Alito041@users.noreply.github.com>
2023-12-12 09:58:45 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+#suma
+a= 41
+b= 18
+print("suma es igual a:", (a+b))
+#suma de variables
+
+#resta
+c= 5.4
+d= 1.1
+print("resta es igual a:", (c-d))
+#resta de variables
+
+#multiplicacion
+e= 7
+f= 3
+print("multiplicacion es igual a:", (e*f))
+#multiplicacion de variables
+
+#division
+g= 82
+h= 2
+print("division es igual a:", (g/h))
+#division de variables
+
+print("+++++++++++++++++++++++++++++++++++++++++")
+
+#nombre y longitud
+nombre= "Alex David Loayza Ramirez"
+#nombre definido con una variable
+longitud= len(nombre)
+#Los caracteres de la variable nombre se contaran con la funcion len
+print("caracteres")
+print("mi nombre es:", nombre," \la longitud es:", longitud)
+
+print("+++++++++++++++++++++++++++++++++++++++++")
+#conversion de datos
+uno= 66
+dos= 38.71
+print("dato original:", type(uno))
+print("dato original 2:", type(dos))
+
+convertir= float(uno)
+convertir2= int(dos)
+
+print("+++++++++++++++++++++++++++++++++++++++++")
+
+print("dato convertir:", type(convertir))
+print("dato convertir2:", type(convertir2))
